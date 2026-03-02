148deb7ef0ad263d9a2deeaa42ea4d05da5cabee
Rose Joseph <rose@Roses-MacBook-Pro.local>
2026-03-02 03:15:19 -0500

    Switch DB From SQLite to PostgreSQL

--- backend/musicmixxer/music_app/music_app/settings.py
+++ backend/musicmixxer/music_app/music_app/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'music_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'musicmixxer_db',
+        'USER': 'rose',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
