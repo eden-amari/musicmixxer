3dff3240ac258c86317bb68dd8ddeac9612132af
Rose Joseph <robojousa@gmail.com>
2026-03-23 22:28:39 -0400

    add track service layer

--- backend/apps/tracks/models.py
+++ backend/apps/tracks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Track(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    bpm = models.FloatField()
+    genre = models.CharField(max_length=100)
+    energy = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.artist}"
